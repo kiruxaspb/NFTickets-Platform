bc8ef87e0b23f46e488dbc908fc4f79778b3810b
flogea <katua.logvinova@gmail.com>
2023-05-23 01:53:37 +0300

    backend add router

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
 const path = require('path');
 const express = require('express');
-const uploader = require('./src/js/pinataTool.js');
-const app = express();
-const QRCode = require('qrcode');
 
-app.use(express.static(path.join(__dirname, 'src')))
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/src/index.html`);
-});
+const app = express();
+
+// app.use(express.static(path.join(__dirname, 'src')));
+// app.get('/', (req, res) => {
+//   res.sendFile(`${__dirname}/src/index.html`);
+// });
+
+// app.use(express.static(path.join(__dirname, './client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile('index.html', { root: path.join(__dirname, './client/build') });
+// });
+
+app.use('/api', require('./routes/route'));
 
 // The response of the token uploader module
-app.get('/api', async function (req, res) {
-  if (!req.query) return res.sendStatus(400)
-  console.log(req.query);
-  const address = req.query.address;
-  await QRCode.toFile('qrcode.png', address, {
-    color: {
-      dark: '#170034',
-      light: '#e238a5'
-    },
-    width: 256
-  }, function (err) {
-    if (err)
-      throw err;
-    console.log('done');
-  })
-  const filename = 'qrcode ' + address.slice(0, 5);
-  const uploaded = await uploader.uploadFile(filename, 'qrcode.png');
-  console.log("Upload result:", uploaded);
-  res.send(uploaded);
-})
 
-app.listen(3000, () => {
-  console.log('Application listening on port 3000!');
-  console.log('Link: http://localhost:3000/')
+app.listen(5000, () => {
+  console.log('Application listening on port 5000!');
+  console.log('Link: http://localhost:5000/');
 });
