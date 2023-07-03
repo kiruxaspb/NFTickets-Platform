7dd5390c4a44a33662884d48dae897609c580ad7
flogea <katua.logvinova@gmail.com>
2023-07-03 12:33:57 +0300

    connection to metamask

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function index({ path, children, className }) {
+function index({ path, children, className, onClick }) {
   return (
     <Link to={path}>
       <>
-        <button className={className}>{children}</button>
+        <button className={className} onClick={onClick}>
+          {children}
+        </button>
       </>
     </Link>
   );
