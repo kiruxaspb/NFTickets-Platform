b5eb328c20f27326be922ec6d8be7aa09b609034
Kirill Dvoretskov <65371121+kiruxaspb@users.noreply.github.com>
2023-07-17 22:22:07 +0300

    Revert "Cleanup package.json & server code (#5)"
    This reverts commit 122b0c10340d5512f5407979470582ed1abae949.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+// app.use(express.static(path.join(__dirname, 'src')));
+// app.get('/', (req, res) => {
+//   res.sendFile(`${__dirname}/src/index.html`);
+// });
+
+// app.use(express.static(path.join(__dirname, './client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile('index.html', { root: path.join(__dirname, './client/build') });
+// });
 
 app.use('/api', require('./routes/route'));
 
