42837246e33000324a0fda5b274b2364b8db1c88
Katua <katua.logvinova@gmail.com>
2023-07-21 20:41:01 +0300

    add event page

--- client/src/components/Input&Label/index.jsx
+++ client/src/components/Input&Label/index.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import './index.scss';
 
-function index({ name, type, onChange, title, value }) {
+function index({ name, type, onChange, title, value, maxLength, min, step }) {
   return (
     <div className={`input__data ${type}`}>
-      <input type={type} name={name} required="required" onChange={onChange} value={value} />
+      <input
+        type={type}
+        name={name}
+        required="required"
+        onChange={onChange}
+        value={value}
+        maxLength={maxLength}
+        min={min}
+        step={step}
+      />
       <span htmlFor={name}>{title}</span>
       <i></i>
     </div>
--- client/src/pages/AddEventPage.jsx
+++ client/src/pages/AddEventPage.jsx
@@ -29,7 +29,13 @@ function AddEventPage() {
             <form className="eventForm">
               <h2 className="howItWorks center">Добавить мероприятие</h2>
               <Input name="name" type="text" onChange={eventInfoHandler} title="Название" />
-              <Input name="symbol" type="text" onChange={eventInfoHandler} title="Символ" />
+              <Input
+                name="symbol"
+                type="text"
+                onChange={eventInfoHandler}
+                title="Символ (Максимум 5 букв)"
+                maxLength="5"
+              />
               <Input
                 name="quantity"
                 type="number"
@@ -42,27 +48,44 @@ function AddEventPage() {
                 onChange={eventInfoHandler}
                 title="Дата мероприятия"
               />
-              <Input name="price" type="number" onChange={eventInfoHandler} title="Цена" />
+              <Input
+                name="price"
+                type="number"
+                onChange={eventInfoHandler}
+                title="Цена"
+                min="0.000005"
+                step="0.000001"
+              />
               <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '30px' }}>
-                <label>Transferable</label>
-                <div>
-                  <input
-                    name="transferable"
-                    type="radio"
-                    id="choice1"
-                    onChange={eventInfoHandler}
-                    value={true}
-                  />
-                  <label htmlFor="choice1">Да</label>
+                <label style={{ fontSize: '20px' }}>Передаваемые</label>
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    fontSize: '20px',
+                  }}>
+                  <div style={{ margin: '5px' }}>
+                    <input
+                      name="transferable"
+                      type="radio"
+                      id="choice1"
+                      onChange={eventInfoHandler}
+                      value={true}
+                    />
+                    <label htmlFor="choice1">Да</label>
+                  </div>
 
-                  <input
-                    name="transferable"
-                    type="radio"
-                    id="choice2"
-                    onChange={eventInfoHandler}
-                    value={false}
-                  />
-                  <label htmlFor="choice2">Нет</label>
+                  <div style={{ margin: '5px' }}>
+                    <input
+                      name="transferable"
+                      type="radio"
+                      id="choice2"
+                      onChange={eventInfoHandler}
+                      value={false}
+                    />
+                    <label htmlFor="choice2">Нет</label>
+                  </div>
                 </div>
               </div>
 
